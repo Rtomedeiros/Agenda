31030ea12eb828cbda61d660b7cf62dddb25388d
fmd00 <felipe.mamede@outlook.com>
2021-02-18 18:21:13 -0300

    Added SQLITE, changed Agenda to a static class

--- Agenda.cs
+++ Agenda.cs
@@ -1,24 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite; // dotnet add package Microsoft.Data.Sqlite
 
-class Agenda {
-    List<Evento> eventos = new List<Evento>();
+static class Agenda {
 
-    public void AdicionarEvento(Evento e) {
-        eventos.Add(e);
-    }
+    static List<Evento> eventos = new List<Evento>();
 
-    public void ListarEventos() {
-        int index = 1;
-        Console.WriteLine("");
-        foreach (Evento e in this.eventos) {
-            Console.Write($"{index}) "); e.PrintEvento();
-            Console.WriteLine("");
-            index++;
+    public static void LoadDatabase() {
+        string cs = "Data Source=:memory:";
+        using SQLiteConnection dbConnection = new SQLiteConnection(cs);
+        dbConnection.Open();
+        SQLiteCommand cmd = dbConnection.CreateCommand();
+        cmd.CommandText = @"CREATE DATABASE IF NOT EXISTS Agenda";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = @"USE Agenda";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Eventos(Evento VARCHAR(255), Data datetime)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = @"SELECT * FROM Eventos";
+        using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+            while (reader.Read()) {
+                string evento = reader.GetString(0);
+                Console.WriteLine($"Hello, {evento}!");
+            }
         }
     }
 
-    public void ExcluirEvento(int excluir) {
+    public static void AdicionarEvento(Evento e) {
+        eventos.Add(e);
+    }
+
+    public static void ListarEventos() {
+        eventos.ForEach(x => Console.WriteLine($"[{eventos.IndexOf(x)}] {x}\n"));
+    }
+
+    public static void ExcluirEvento(int excluir) {
         int index = 1;
         foreach (Evento i in eventos) {
             if (index == excluir) {
@@ -27,5 +44,30 @@ class Agenda {
             }
             index++;
         }
+    }
+
+    public static void CriarEvento() {
+        Console.WriteLine("Digite o dia do evento: ");
+        int dia = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o mês do evento: ");
+        int mes = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o ano do evento: ");
+        int ano = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a descrição do evento: ");
+        string descricao = Console.ReadLine();
+        try {
+            DateTime dt = new DateTime(ano, mes, dia);
+            Evento novo = new Evento(dt, descricao);
+            Agenda.AdicionarEvento(novo);
+        } catch {
+            Console.WriteLine("Você digitou algum valor incorreto, tente novamente.");
+        }
+    }
+
+    public static void ExcluirEvento() {
+        Console.WriteLine("Digite o evento que deseja excluir: ");
+        int idExcluir = Convert.ToInt32(Console.ReadLine());
+        Evento excluir = Agenda.eventos[idExcluir];
+        Agenda.eventos.Remove(excluir);
     }
 }
--- Projeto.cs
+++ Projeto.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+
 
 namespace ProjetoAgenda {
     class Program {
         static void Main(string[] args) {
-            Agenda agenda = new Agenda();
-            bool MostrarMenu = true;
+            Agenda.LoadDatabase();
+
             short MenuChoice = 0;
-            while (MostrarMenu) {
+            while (true) {
                 Console.WriteLine("---- Projeto Agenda ----");
                 Console.WriteLine("Digite os números a esquerda para navegar o menu:");
                 Console.WriteLine("1 - Listar datas.");
@@ -16,43 +16,15 @@ namespace ProjetoAgenda {
                 Console.WriteLine("4 - Sair.");
                 MenuChoice = Convert.ToInt16(Console.ReadLine());
                 if (MenuChoice == 1) {
-                    agenda.ListarEventos();
+                    Agenda.ListarEventos();
                 } else if (MenuChoice == 2) {
-                    CriarEvento(agenda);
+                    Agenda.CriarEvento();
                 } else if (MenuChoice == 3) {
-                    ExcluirEvento(agenda);
+                    Agenda.ExcluirEvento();
                 } else if (MenuChoice == 4) {
-                    MostrarMenu = false;
+                    break;
                 } else Console.WriteLine("Digite uma opção válida.");
             }
-        }
-
-        static void ListarDatas(Agenda agenda) {
-            agenda.ListarEventos();
-        }
-
-        static void CriarEvento(Agenda agenda) {
-            Console.WriteLine("Digite o dia do evento: ");
-            int dia = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o mês do evento: ");
-            int mes = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o ano do evento: ");
-            int ano = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite a descrição do evento: ");
-            string descricao = Console.ReadLine();
-            try {
-                DateTime dt = new DateTime(ano, mes, dia);
-                Evento novo = new Evento(dt, descricao);
-                agenda.AdicionarEvento(novo);
-            } catch {
-                Console.WriteLine("Você digitou algum valor incorreto, tente novamente.");
-            }
-        }
-
-        static void ExcluirEvento(Agenda agenda) {
-            Console.WriteLine("Digite o evento que deseja excluir: ");
-            int excluir = Convert.ToInt32(Console.ReadLine());
-            agenda.ExcluirEvento(excluir);
         }
     }
 }
