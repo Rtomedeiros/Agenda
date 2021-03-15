31ba941c17dd8f716543261b69d40b62f28c522f
fmd00 <felipe.mamede@outlook.com>
2021-03-15 14:44:04 -0300

    Updated UI from console to forms

---
+++ Agenda/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateListBox();
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this.monthCalendar1.SelectionStart, this);
+            form2.ShowDialog();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if(listBox.SelectedItem.ToString() != "")
+            {
+                Evento evento = AgendaInterface.eventos.Find(x =>
+                {
+                    if (x.Data == monthCalendar1.SelectionStart && x.Descricao == listBox.SelectedItem.ToString()) return true;
+                    else return false;
+                });
+                AgendaInterface.eventos.Remove(evento);
+                Database.DatabaseDelete(evento);
+                UpdateListBox();
+            } 
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            List<string> eventosDia = new List<string>();
+            eventosDia.Clear();
+            AgendaInterface.eventos.ForEach(x =>
+            {
+                if (monthCalendar1.SelectionStart == x.Data) eventosDia.Add(x.Descricao);
+            });
+            listBox.DataSource = eventosDia;
+            listBox.Update();
+        }
+
+        public void UpdateListBox()
+        {
+            List<string> eventosDia = new List<string>();
+            eventosDia.Clear();
+            AgendaInterface.eventos.ForEach(x =>
+            {
+                if (monthCalendar1.SelectionStart == x.Data) eventosDia.Add(x.Descricao);
+            });
+            listBox.DataSource = eventosDia;
+            listBox.Update();
+        }
+    }
+}
---
+++ Agenda/src/AgendaInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+static class AgendaInterface
+{
+    //Classe Agenda
+    public static List<Evento> eventos = new List<Evento>();
+
+    //Método Adicionar evento
+    public static void AdicionarEvento(Evento e)
+    {
+        eventos.Add(e);
+    }
+
+    public static void SortByDate()
+    {
+        eventos.Sort((x, y) => y.Data.CompareTo(x.Data));
+        eventos.Reverse();
+    }
+
+    //Método Deletar evento
+    public static void DeletarEvento(int index)
+    {
+        if (index < 1 || index > eventos.Count)
+        { // [0] [1]
+            Console.WriteLine("Este evento não existe.1");
+            return;
+        }
+        Evento deletar = eventos[index - 1];
+        if (deletar == null)
+        {
+            Console.WriteLine("Este evento não existe.2");
+            return;
+        }
+        eventos.Remove(deletar);
+        Database.DatabaseDelete(deletar);
+    }
+}
